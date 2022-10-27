0d3f7704fdd07e74c7abc223cfbbb64f9bac9d0b
Ajmian-abrar <114634466+Ajmian-abrar@users.noreply.github.com>
2022-10-27 10:56:17 +0600

    Add files via upload

---
+++ labtask4(22-46340-1)1.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+
+class Stack
+{
+   int top;
+   public:
+   int myStack[10];
+
+   Stack() { top = -1; }
+   bool push(int x);
+   int pop();
+   bool isEmpty();
+};
+
+   bool Stack::push(int item)
+   {
+      if (top >= (10-1)) {
+      cout << "Stack Overflow!!!";
+      return false;
+   }
+else {
+   myStack[++top] = item;
+   cout<<item<<endl;
+   return true;
+   }
+}
+
+
+int Stack::pop()
+{
+   if (top < 0) {
+      cout << "Stack Underflow!!";
+      return 0;
+   }
+else {
+       int item = myStack[top--];
+      return item;
+   }
+}
+
+
+bool Stack::isEmpty()
+{
+   return (top < 0);
+}
+
+
+int main()
+{
+   class Stack stack;
+   cout<<"The Stack Push "<<endl;
+   stack.push(2);
+   stack.push(4);
+   stack.push(6);
+   stack.push(8);
+   stack.push(9);
+   cout<<"The Stack Pop : "<<endl;
+   while(!stack.isEmpty())
+      {
+      cout<<stack.pop()<<endl;
+      }
+   return 0;
+}
